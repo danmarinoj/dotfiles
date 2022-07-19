cd1c97f24572835ce8fc0d0453ebcc896d96d5c2
Daniel Orlando Marino Johnson <daniel@marino-johnson.org>
2022-07-19 13:57:29 -0400

    Remove junk from emacs file

--- .emacs.el
+++ .emacs.el
@@ -4,9 +4,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
 ;; recent files
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
@@ -31,19 +28,3 @@
 (autoload 'maxima "maxima" "Maxima interaction" t)
 (autoload 'imath-mode "imath" "Imath mode for math formula input" t)
 (setq imaxima-use-maxima-mode-flag t)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
- '(package-selected-packages '(magit solarized-theme matlab-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
